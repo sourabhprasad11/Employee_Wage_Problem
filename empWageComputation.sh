8501788e06cfd3508deee65a07a638f623879b5a
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-09 14:38:15 +0530

    Computes Monthly Wage of the Employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,14 +5,15 @@ echo "Welcome to the Employee's Wage Computation";
 WAGE_PER_HR=20
 FULL_DAY_HR=8
 PART_TIME_HR=4
+DAYS=20
 
 x=$((RANDOM%3))
 case $x in
-1)        wage=$(( $FULL_DAY_HR*$WAGE_PER_HR ))
-          echo "The Full time salary is $wage";
+1)        wage=$(( $DAYS*$FULL_DAY_HR*$WAGE_PER_HR ))
+          echo "The Monthly Full-time salary is $wage";
                         ;;
-2)        wage=$(( $PART_TIME_HR*$WAGE_PER_HR ))
-          echo "The Part Time salary is $wage";
+2)        wage=$(( $DAYS*$PART_TIME_HR*$WAGE_PER_HR ))
+          echo "The Monthly Part-time salary is $wage";
                         ;;
 *) 	  wage=0
    	  echo "the salary for no working is $wage";
