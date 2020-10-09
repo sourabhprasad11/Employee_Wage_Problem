7e8fea896a7e797f49c5df04e8e4148e7fe67b71
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-09 12:59:53 +0530

    Shows that the Employee Attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,3 +2,12 @@
 
 echo "Welcome to the Employee's Wage Computation";
 
+x=$((RANDOM%2))
+
+if [ $x -eq 1 ]; then
+        echo "Employee is Present";
+else
+        echo "Employee is Absent";
+fi
+
+
