a0ca263d9944677c0022ce54fc5085a07700c358
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-09 13:12:03 +0530

    Computes Daily Wage of the Employees

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,12 +2,19 @@
 
 echo "Welcome to the Employee's Wage Computation";
 
-x=$((RANDOM%2))
+WAGE_PER_HR=20
+FULL_DAY_HR=8
 
+x=$((RANDOM%2))
 if [ $x -eq 1 ]; then
         echo "Employee is Present";
+	dailywage=$(( $WAGE_PER_HR*$FULL_DAY_HR ));
+	echo "The Daily Wage: $dailywage";
 else
         echo "Employee is Absent";
+	dailywage=0;
+	echo "The Daily Wage: $dailywage";
 fi
+
 
 
